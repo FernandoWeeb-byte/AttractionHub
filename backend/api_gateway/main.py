073f21c5008c7574a436dd9cebeb6d2801a94b5d
FernandoWeeb-byte <fernandobr7186@gmail.com>
2021-11-11 01:29:28 -0300

    init user

--- backend/api_gateway/main.py
+++ backend/api_gateway/main.py
@@ -1,23 +1,12 @@
 from flask.app import Flask
 import flask_restful as fr
 from flask_restful import reqparse
+import ApiGateway
 app = Flask('API_gateway')
 
 api = fr.Api(app)
 
-class Api_Gateway(fr.Resource):
-    def get(self):
-        return {
-            'data': 'aloalo',
-        }, 400
+api.add_resource(ApiGateway, '/api_gateway')
 
-    def post(self):
-        parser = reqparse.RequestParser()
-        parser.add_argument('name', required=True)
-        args = parser.parse_args()
-        return args, 200
-
-api.add_resource(Api_Gateway, '/api_gateway')
-
-app.run()
+app.run(threaded=True)
 
