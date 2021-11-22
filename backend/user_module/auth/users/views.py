d2f2fa6a93b9ab3b414187b8f4155fb83a2af6a2
Giulia Maia <gmaiaserafim@gmail.com>
2021-11-22 20:07:14 -0300

    get_user, logout e jwt funcionando

--- backend/user_module/auth/users/views.py
+++ backend/user_module/auth/users/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.serializers import Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import User
 from .serializers import UserSerializer
+
+import jwt, datetime
 # Create your views here.
 
 class RegisterView(APIView):
@@ -26,6 +29,47 @@ class LoginView(APIView):
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect password!')
 
-        return Response({
-            'message': 'sucess'
-        })
+        payload = {
+            'id': user.id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
+            'iat': datetime.datetime.utcnow()
+        }
+
+        token = jwt.encode(payload, 'secret', algorithm='HS256')
+
+        response = Response()
+        
+        response.set_cookie(key='token', value=token, httponly=True)
+        
+        response.data = {
+            'token': token
+        }
+        
+        return response
+
+
+class UserView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('token')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated!')
+
+        try:
+            payload = jwt.decode(token, 'secret', algorithms=['HS256'])
+        except jwt.ExpiredSignatureError:
+             raise AuthenticationFailed('Unauthenticated!')
+        
+        user = User.objects.filter(id=payload['id']).first()
+
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+class LogoutView(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('token')
+        response.data = {
+            'message': 'success'
+        }
+
+        return response
