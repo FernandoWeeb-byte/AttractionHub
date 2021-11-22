5f9408a87093fcc68604c3ab937a3ad6158c7af1
Giulia Maia <gmaiaserafim@gmail.com>
2021-11-21 21:56:05 -0300

    modulo do usuario com login e registro

---
+++ backend/user_module/auth/users/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+# Create your views here.
+
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+class LoginView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.get(email=email)
+        
+        if user is None: 
+            raise AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+
+        return Response({
+            'message': 'sucess'
+        })
