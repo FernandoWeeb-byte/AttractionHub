848ab32694a5f33e314a5e6ef62bedc57276e39c
Lucas Lins <lucas_flins@hotmail.com>
2021-11-27 19:22:20 -0300

    new Manager module

---
+++ backend/search/listManager/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Genre(models.Model):
+    title = models.CharField(max_length=64)
+    pass
+
+class Attraction(models.Model):
+    title = models.CharField(max_length=256)
+    enTitle = models.CharField(max_length=256, null=True)
+    desc = models.TextField()
+    urlImg = models.URLField()
+    attractionType = models.CharField(max_length=100)
+    rating = models.CharField(max_length=100)
+    genre = models.ManyToManyField(Genre)
+    score = models.PositiveIntegerField()
+    status = models.CharField(max_length=100)
